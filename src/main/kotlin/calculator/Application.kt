0e20f207f529e6e37e7306137cfacc644f2ff97e
지원 <cheerfulboys@naver.com>
2024-11-15 17:48:24 +0900

    feat(Application): 빈 문자열일 때 0을 출력하는 기능 추가

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -2,9 +2,98 @@ package calculator
 
 import camp.nextstep.edu.missionutils.Console
 
-fun main() {
+fun useTryCatch() {
+    try {
+        val input = Console.readLine()
+        val splitedInput = input.split(",", ":")
+        val numbers: List<Int> = splitedInput.map { it.toInt() }
+        println("결과 : ${numbers.sum()}")
+    } catch (e: NoSuchElementException) {
+        println("결과 : 0")
+    }
+}
+
+fun useCompare() {
     val input = Console.readLine()
-    val splitedInput = input.split(",", ":")
-    val numbers: List<Int> = splitedInput.map { it.toInt() }
-    println("결과 : ${numbers.sum()}")
+    if (input == "") {
+        println("결과 : 0")
+    } else {
+        val splitedInput = input.split(",", ":")
+        val numbers: List<Int> = splitedInput.map { it.toInt() }
+        println("결과 : ${numbers.sum()}")
+    }
+}
+
+fun useNull() {
+    val input = Console.readLine()
+    if (input == null) {
+        println("결과 : 0")
+    } else {
+        val splitedInput = input.split(",", ":")
+        val numbers: List<Int> = splitedInput.map { it.toInt() }
+        println("결과 : ${numbers.sum()}")
+    }
+}
+
+fun useIsBlank() {
+    val input = Console.readLine()
+    if (input.isBlank()) {
+        println("결과 : 0")
+    } else {
+        val splitedInput = input.split(",", ":")
+        val numbers: List<Int> = splitedInput.map { it.toInt() }
+        println("결과 : ${numbers.sum()}")
+    }
+}
+
+fun useIsEmpty() {
+    val input = Console.readLine()
+    if (input.isEmpty()) {
+        println("결과 : 0")
+    } else {
+        val splitedInput = input.split(",", ":")
+        val numbers: List<Int> = splitedInput.map { it.toInt() }
+        println("결과 : ${numbers.sum()}")
+    }
+}
+
+fun useIsNullOrBlank() {
+    val input = Console.readLine()
+    if (input.isNullOrBlank()) {
+        println("결과 : 0")
+    } else {
+        val splitedInput = input.split(",", ":")
+        val numbers: List<Int> = splitedInput.map { it.toInt() }
+        println("결과 : ${numbers.sum()}")
+    }
+}
+
+fun useIsNullOrEmpty() {
+    val input = Console.readLine()
+    if (input.isNullOrEmpty()) {
+        println("결과 : 0")
+    } else {
+        val splitedInput = input.split(",", ":")
+        val numbers: List<Int> = splitedInput.map { it.toInt() }
+        println("결과 : ${numbers.sum()}")
+    }
+}
+
+fun main() {
+//    useTryCatch()
+//    useCompare()
+//    useNull()
+//    useIsBlank()
+//    useIsEmpty()
+//    useIsNullOrBlank()
+//    useIsNullOrEmpty()
+
+    try {
+        val input = Console.readLine()
+        val splitedInput = input.split(",", ":")
+        val numbers: List<Int> = splitedInput.map { it.toInt() }
+        println("결과 : ${numbers.sum()}")
+    } catch (e: NoSuchElementException) {
+        println("결과 : 0")
+    }
 }
