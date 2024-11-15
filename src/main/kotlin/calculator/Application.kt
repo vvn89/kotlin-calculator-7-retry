c25fc74006fde6950f1a7d52c1672ca97e6ec336
지원 <cheerfulboys@naver.com>
2024-11-15 17:22:57 +0900

    feat(Application): 기본 구분자로 문자열을 더하는 기능 추가

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -1,5 +1,10 @@
 package calculator
 
+import camp.nextstep.edu.missionutils.Console
+
 fun main() {
-    // TODO: 프로그램 구현
+    val input = Console.readLine()
+    val splitedInput = input.split(",", ":")
+    val numbers: List<Int> = splitedInput.map { it.toInt() }
+    println("결과 : ${numbers.sum()}")
 }
